4f52c57ab47c291b086f82322d59d9ac4dd8a3df
Javoxir <ist8797@gmail.com>
2023-06-10 17:05:00 +0500

    first commit

---
+++ javoxirbek.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//Arrays - massivlar
+	
+	int a[5]={24,32,56,89,84};
+	
+	for(int b=0; b<5; b++){
+		cout<<"a["<<b<<"]"<<a[b]<<endl;
+	}
+	
+	
+	
+	return 0;
+}
